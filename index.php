d8116b20f321516d308dee423a3dc57bf722b790
ican-bangcat <ikhsan23ti@mahasiswa.pcr.ac.id>
2024-06-23 06:26:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+require 'core/koneksi.php';
+$conn = koneksiDatabase();
+
+if (isset($_POST["login"])) {
+    $username = mysqli_real_escape_string($conn, $_POST["username"]);
+    $password = $_POST["password"];
+    $passwordHashed = hash('sha256', $password);
+
+    $result = mysqli_query($conn, "SELECT * FROM security WHERE username='$username'");
+
+    // cek username
+    if (mysqli_num_rows($result) === 1) {
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if ($passwordHashed === $row["password"]) {
+            header("location: tes.php");
+            exit;
+        }
+    }
+    $error=true;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Login Droppaket</title>
+</head>
+
+<body>
+
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="row border rounded-5 p-3 bg-white shadow box-area">
+            <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
+                <div class="featured-image mb-3">
+                    <img src="images/tofi.png" class="img-fluid" style="width: 180px;">
+                </div>
+            </div>
+            <div class="col-md-6 right-box">
+                <div class="row align-items-center">
+                    <div class="header-text mb-4">
+                        <h2>SELAMAT DATANG</h2>
+                        <p>silahkan masukkan username dan password anda</p>
+                    </div>
+                    <!-- LOGIN BOSQ -->
+                    <form action="" method="post">
+                        <div class="input-group mb-3">
+                            <input type="text" name="username" class="form-control form-control-lg bg-light fs-6" placeholder="Username">
+                        </div>
+                        <div class="input-group mb-1">
+                            <input type="password" name="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                        </div>
+                        <div class="input-group mb-5 d-flex justify-content-between">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="formCheck">
+                                <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
+                            </div>
+                            <div class="forgot">
+                                <small><a href="#">Lupa Password?</a></small>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button type="submit" name="login" class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                        </div>
+                        <div class="input-group mb-3">
+                            <button class="btn btn-lg btn-light w-100 fs-6"><img src="images/google.png" style="width:20px" class="me-2"><small>Sign In with Google</small></button>
+                        </div>
+                        <?php if (isset($error)) : ?>
+                            <p style="color: red; font-style: italic;">username / password salah</p>
+                        <?php endif; ?>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
